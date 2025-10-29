0fb0fdc824238180bb2d184d30ba05281022fcec
Phantasmal24 <aditykr241106@gmail.com>
2025-10-29 15:03:05 +0530

    Initial commit utils.js

---
+++ Git-Stash-Practice/utils.js
@@ -0,0 +1,13 @@
+// Utils.js
+
+// A function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// A function to say hello
+function sayHello(name) {
+    console.log("Hello, " + name + "!");
+    // OOPS i'm not finished with this function yet....
+    // I still need to add error checking...
+}
