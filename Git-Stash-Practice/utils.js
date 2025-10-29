8be30a4d5ecf6f0cd004bf5838001e3b1a411bbc
Phantasmal24 <aditykr241106@gmail.com>
2025-10-29 15:43:05 +0530

    Stash Notes: This contains stash notes

--- Git-Stash-Practice/utils.js
+++ Git-Stash-Practice/utils.js
@@ -1,13 +1,12 @@
-// Utils.js
-
-// A function to add two numbers
-function add(a, b) {
-    return a + b;
-}
-
-// A function to say hello
-function sayHello(name) {
-    console.log("Hello, " + name + "!");
-    // OOPS i'm not finished with this function yet....
-    // I still need to add error checking...
-}
+/** Type of stashes and their uses.
+ * 1. git stash -> Hides tracked files.
+ * 2. git stash -u -> Hides tracked + untracked files.
+ * 3. git stash -a -> Hides tracked + untracked + ignored files.
+ * 4. git stash pop -> Applies the latest stash and deletes it.
+ * 5 git stash list -> Lists all stashes.
+ * 6. git stash apply 'stash@{n}' -> Applies a specific stash and keeps it.
+ * 7. git stash drop 'stash@{n}; -> Deletes a specific stash.
+ * 8. git stash -p -> Lets you partially stash changes inside a file.
+ * 9. git stash branch 'branch_name' -> Creates a new branch from a stash.
+ * 10. git stash clear -> Deletes all stashes.
+ */
