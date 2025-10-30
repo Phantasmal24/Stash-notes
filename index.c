f23ce82dda23fcc3bc5b5a3e4593f504c2239bc0
Aditya Kumar <adityakr241106@gmail.com>
2025-10-30 12:01:46 +0530

    Simple C program to sum two numbers
    two Program adding script in C langauge

---
+++ index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int a, b;
+  printf("Enter the number a: ");
+  scanf("%d", &a);
+  printf("Enter the number b: ");
+  scanf("%d", &b);
+  printf("The sum of %d %d is: %d\n", a, b, a + b);
+  return 0;
+}
